4541413908835637d06faaf8581a5c777150cd0d
Lukas -krtek.net- Novy <lukas.novy@pirati.cz>
2014-10-10 03:56:42 -0400

    Bordel jak Brno, sorry...

--- js/game_manager.js
+++ js/game_manager.js
@@ -17,11 +17,11 @@ function GameManager(size, InputManager, Actuator) {
   this.inputManager.on('run', function() {
     if (this.running) {
       this.running = false;
-      this.actuator.setRunButton('Auto-run');
+      this.actuator.setRunButton('Onderku');
     } else {
       this.running = true;
       this.run()
-      this.actuator.setRunButton('Stop');
+      this.actuator.setRunButton('<i>Hraje </i> Roman...');
     }
   }.bind(this));
 
--- js/html_actuator.js
+++ js/html_actuator.js
@@ -107,7 +107,7 @@ HTMLActuator.prototype.updateScore = function (score) {
 
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
-  var message = won ? "You win!" : "Game over!"
+  var message = won ? "Je to tam!" : "Na kluka z Brno dobrý!"
 
   // if (ga) ga("send", "event", "game", "end", type, this.score);
 
